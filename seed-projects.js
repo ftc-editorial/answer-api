7a1b81865e8255d966ad76a8163a793555ad82b8
Ændrew Rininsland <andrew.rininsland@ft.com>
2016-12-12 17:04:50 +0000

    Updating project seeder

--- seed-projects.js
+++ seed-projects.js
@@ -1,4 +1,9 @@
 #!/usr/local/env node
 const seed = require('./lib/seed'); // eslint-disable-line global-require
 
-seed({ force: true, log: true, useTestData: false });
+seed({
+  force: false,
+  log: true,
+  useTestData: false,
+  seedProjects: true
+});
