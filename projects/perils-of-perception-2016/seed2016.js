935641aa79f0246028d6c7653735c22d434570ea
Ændrew Rininsland <andrew.rininsland@ft.com>
2016-12-14 14:17:01 +0000

    Updating Perils2016 seeder

--- projects/perils-of-perception-2016/seed2016.js
+++ projects/perils-of-perception-2016/seed2016.js
@@ -4,7 +4,9 @@
 
 const Project = require('../../lib/api/project/model');
 const Question = require('../../lib/api/question/model');
+const Response = require('../../lib/api/response/model');
 
+const BATCH_2622312_DATA = require('././Batch_2622312_batch_results.json');
 const SURVEY_QUESTIONS = require('./import-ipsos-2016')();
 
 module.exports = async function seedPerils() {
@@ -20,7 +22,7 @@ module.exports = async function seedPerils() {
       },
     }).spread(i => i);
 
-    return await Promise.all(
+    const qs = await Promise.all(
       SURVEY_QUESTIONS.map(async q => await (
           await Question.findCreateFind({
             where: {
@@ -33,6 +35,52 @@ module.exports = async function seedPerils() {
         .setProject(p)
       )
     );
+
+    return BATCH_2622312_DATA.filter(r => !r.reject).map(async r => await Promise.all(
+      qs.map(async (q) => {
+        const qNum = q.title.match(/Q(\d{1,2})\.\s/)[1];
+        let i;
+        switch (qNum) {
+          case '1':
+          case '2':
+          case '3':
+          case '4':
+          case '5':
+          case '6':
+          case '7':
+            i = qNum;
+            break;
+          case '10':
+            i = '9';
+            break;
+          case '12':
+            i = '11';
+            break;
+          case '14':
+            i = '13';
+            break;
+          case '16':
+            i = '15';
+            break;
+          default:
+            i = false;
+            break;
+        }
+
+        if (!i) return;
+
+        await (await Response.findCreateFind({
+          where: {
+            'meta.HITId': r.HITId,
+            questionId: q.id,
+          },
+          defaults: {
+            value: r.Answer[`q${i}`],
+            submitted: new Date('2016-12-09T00:00:00'),
+            meta: r,
+          },
+        }).spread(inst => inst)).setQuestion(q);
+      })));
   } catch (e) {
     console.error(e);
   }
